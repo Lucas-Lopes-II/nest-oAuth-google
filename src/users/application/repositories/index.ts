6f867b3c2a29cfd4ee17b75c54f454cdeaa44cc6
Lucas Lopes <lucas.santos.pessoal@outlook.com>
2023-10-12 13:16:29 -0300

    feat: configuração do repositório de user

--- src/users/application/repositories/index.ts
+++ src/users/application/repositories/index.ts
@@ -1,1 +1,3 @@
 export * from './user-repository.interface';
+export * from './typeorm/user-typeorm.repository';
+// export * from './';
--- src/users/infra/users.module.ts
+++ src/users/infra/users.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, getDataSourceToken } from '@nestjs/typeorm';
+
+import { DataSource } from 'typeorm';
 import { UsersController } from './users.controller';
+import { UserEntity } from './entities/user.entity';
+import { IUserRepository, UserRepository } from '../application/repositories';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [],
+  providers: [
+    {
+      provide: IUserRepository,
+      useFactory: (dataSource: DataSource) => {
+        return UserRepository.createInstance(
+          dataSource.getRepository(UserEntity),
+        );
+      },
+      inject: [getDataSourceToken()],
+    },
+  ],
+  exports: [IUserRepository],
 })
 export class UsersModule {}
